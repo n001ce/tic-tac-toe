7d27f31ba7b8a88050ed74067c71cf3515ca8a7d
n001ce <a1ccormier@gmail.com>
2021-06-30 01:49:18 -0400

    Attempted to add a soundclip to the buttons when clicked

--- js/app.js
+++ js/app.js
@@ -1,16 +1,9 @@
 const player = new Object;
 let opponent;
-const canvas = document.getElementById("cvs")
-const ctx = canvas.getContext("2d")
-const options=document.querySelector('.options')
+const options = document.querySelector('.options')
 const computerBtn = document.querySelector('.computer')
-const friendBtn = options.querySelector(".friend");
-const xBtn = options.querySelector(".x")
-const oBtn = options.querySelector(".o")
-const playBtn = options.querySelector(".play")
+const friendBtn = options.querySelector(".player")
 
-ctx.fillRect(150, 100, 50, 50)
-ctx.strokeRect(250, 200, 50, 50)
 
 computerBtn.addEventListener("click", function(){
 	opponent = computer
@@ -18,30 +11,7 @@ computerBtn.addEventListener("click", function(){
 })
 
 friendBtn.addEventListener("click", function(){
-	player.man="O"
-	player.friend="X"
 	switchActive(xBNtn, oBtn)
-})
-
-xBtn.addEventListener("click", function(){
-	player.man="X"
-	player.computer="O"
-	player.friend ="O"
-})
-
-playBtn.addEventListener("click", function(){
-	if(!opponent){
-		computerBtn.style.backgroundColor="red"
-		friendBtn.style.background ="red"
-		return
-	}
-	if(!player.man){
-		oBtn.style.background="red"
-		xBtn.style.background="red"
-		return		
-	}
-	init(player, opponent)
-	options.classList.add('hide')
 })
 
 
@@ -49,3 +19,5 @@ function switchActive(off, on){
 	off.classList.remove("active")
 	on.classList.add("active")
 }
+
+function play(){document.getElementById("audio").play()}
